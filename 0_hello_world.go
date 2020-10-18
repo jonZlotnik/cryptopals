8b276291e2551047ece0f52592b1490c95278229
Jon Zlotnik <j.zlotnik@live.ca>
2020-10-17 23:51:34 -0400

    separate test file

---
+++ 0_hello_world.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+)
+
+// HelloWorld ...
+func HelloWorld() {
+	fmt.Println("Hello, World!")
+}
--- 0_hello_world_test.go
+++ 0_hello_world_test.go
@@ -1,12 +1,5 @@
 package main
 
-import (
-	"fmt"
-)
-
-func HelloWorld() {
-	fmt.Println("Hello, World!")
-}
 func ExampleHelloWorld() {
 	HelloWorld()
 	// Output:
